d725f6170a6c6d2107c8b3a20dfcbf824b93d0af
w3bartisan <zeutsopgemael@gmail.com>
2025-02-01 15:56:17 +0100

    correction du devoir

---
+++ poo-java/src/Etudiant.java
@@ -0,0 +1,47 @@
+
+public class Etudiant extends Personne {
+	
+	private String matricule;
+	private String filiere;
+	private int niveau;
+	public Etudiant() {
+		super("");
+		this.matricule="CM1234" ;
+		this.filiere="INFO";
+		this.niveau=4;
+	}
+	public Etudiant(String name, int age, String matricule, String filiere, int niveau) {
+		super(name, age);
+		this.matricule = matricule;
+		this.filiere = filiere;
+		this.niveau = niveau;
+	}
+	public String getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+	public String getFiliere() {
+		return filiere;
+	}
+	public void setFiliere(String filiere) {
+		this.filiere = filiere;
+	}
+	public int getNiveau() {
+		return niveau;
+	}
+	public void setNiveau(int niveau) {
+		this.niveau = niveau;
+	}
+	
+    @Override
+	public String toString() {
+		return "Etudiant [matricule = " + matricule + ", filiere =" + filiere + ", niveau =" + niveau + ", nom ="
+				+ getName() + ", Age =" + getAge() + "]";
+	}
+	
+
+	
+
+}
--- poo-java/src/Main.java
+++ poo-java/src/Main.java
@@ -1,9 +1,12 @@
 
+import java.util.ArrayList ;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		/*
+		
 		//Creation d'un objet personne
 		Personne p = new Personne("AURIOL", 25);
 		//afficher les propriétés
@@ -11,7 +14,7 @@ public class Main {
 		//System.out.println("l'age est: "+ p.getAge());
 		///
 		///
-		 */
+		 
 		//creation d'un enseignant
 		
 		Enseignant e = new Enseignant("AURIOL", 25, "DOCTEUR", "Master");
@@ -22,6 +25,48 @@ public class Main {
 		System.out.println("le diplome est:"+ e.getDiplome());
 		System.out.println("le nom est:"+ e.getName());
 		System.out.println("l'age est:"+ e.getAge());
+		
+		
+		
+		// Creation d'un etudiant
+		
+		Etudiant et = new Etudiant ("AURIOL", 25,"CMUDS-25SCI2000","INFORMATIQUE",4);
+	System.out.println(et.toString());
+		
+		
+		// liste d'etudiants en java
+	 
+		List<Etudiant> listEtud = new ArrayList<>();
+		
+		listEtud.add(new Etudiant ("MBAPPE", 25,"CMUDS-25SCI2500","INFORMATIQUE",4));
+		listEtud.add(new Etudiant ("PRO", 25,"CMUDS-25SCI2080","INFORMATIQUE",4));
+		listEtud.add(new Etudiant ("VINI", 25,"CMUDS-25SCI2040","INFORMATIQUE",4));
+		listEtud.add(new Etudiant ("BLLGHM", 25,"CMUDS-25SCI2300","INFORMATIQUE",4));
+		
+		System.out.println("les etudiants sont:");
+		
+		
+		// affichage en utilisant la boucle for
+		
+		for (int i=0; i<listEtud.size();i++) {
+			Etudiant e1 = listEtud.get(i);
+			System.out.println(e1.toString());
+		}
+		
+		// Affichage en utilisant la boucle forEach
+		
+	//	for (Etudiant etudiant : listEtud) {
+	//		System.out.println(etudiant.toString());
+	//	}
+		
+		//Affichage en utilisant la map
+		
+		System.out.println(listEtud);
+		
+		listEtud.remove(1);
+		System.out.println(listEtud);
+		
+		
 		
 	}
 }
